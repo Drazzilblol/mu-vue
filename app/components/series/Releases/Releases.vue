419e28c926b29d8d9d5609ca22ca1ddeac4d8cc9
AndrewPretskailo <andrei@dev-jane.com>
2026-06-09 13:27:50 +0300

    update bookmark design

--- app/components/series/Releases/Releases.vue
+++ app/components/series/Releases/Releases.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import dayjs from "dayjs";
 import type { TRequestMeta } from "~/types/General";
 import type { TRelease } from "~/types/Releases";
 
@@ -31,8 +32,8 @@ const addBookmark = async (
 
 <template>
   <div>
-    <div class="custom-block-border !p-4 flex flex-col gap-2">
-      <div class="grid grid-cols-[100px_200px_1fr_20px] gap-2 p-2">
+    <div class="custom-block-border !p-4 flex flex-col gap-3">
+      <div class="grid grid-cols-[100px_200px_1fr] gap-2 p-2">
         <div class="font-bold">Date</div>
         <div class="font-bold">Volume/Chapter</div>
         <div class="font-bold">Groups</div>
@@ -51,10 +52,17 @@ const addBookmark = async (
         "
         v-for="release in releases"
         :key="release.record.id"
-        class="grid grid-cols-[100px_200px_1fr_20px] gap-x-2 even:bg-accent px-2 py-1 rounded"
+        class="grid grid-cols-[100px_200px_1fr] gap-x-2 px-4 py-4 rounded-xl cursor-pointer relative"
+        :class="
+          +releasesStore.bookmark?.release_id === release.record.id
+            ? 'border border-primary hover:border-primary/60'
+            : 'border border-border/40 hover:border-border/80'
+        "
       >
-        <div>{{ release.record.release_date }}</div>
-        <div>
+        <div class="self-center">
+          {{ dayjs(release.record.release_date).format("DD.MM.YYYY") }}
+        </div>
+        <div class="self-center">
           <span v-if="release.record.volume">
             v.{{ release.record.volume }}
           </span>
@@ -62,22 +70,21 @@ const addBookmark = async (
             c.{{ release.record.chapter }}
           </span>
         </div>
-        <div>
+        <div class="self-center">
           <span
             v-for="value in release.record.groups"
             :key="value.group_id"
             class="[&:not(:first-child)]:ml-2 cursor-pointer hover:text-link underline"
-            @click="() => navigateTo(`/group/${value.group_id}`)"
+            @click.stop="() => navigateTo(`/group/${value.group_id}`)"
           >
             {{ value.name }}
           </span>
         </div>
-        <div>
-          {{
-            +releasesStore.bookmark?.release_id === release.record.id
-              ? "+"
-              : "-"
-          }}
+        <div
+          v-if="+releasesStore.bookmark?.release_id === release.record.id"
+          class="absolute right-4 -top-2 bg-primary rounded-full px-3 text-xs"
+        >
+          You stopped here
         </div>
       </div>
     </div>
